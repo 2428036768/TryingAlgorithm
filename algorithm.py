f13d95c85f3314c682426e5f0ce803f3f0be50a2
wy <2428036768@qq.com>
2020-09-18 16:51:53 +0800

    knapsack0_1

--- algorithm.py
+++ algorithm.py
@@ -39,7 +39,25 @@ class Dynamic_programming:
                     dp[i] = min(dp[i], dp[i-coin]+1)
             return dp[amount]
 
+    # 0/1背包问题：给你一个可装载重量为W的背包和N个物品，每个物品有重量和价值两个属性。其中第i个物品的重量为wt[i]，价值为val[i]，现在让你用这个背包装物品，最多能装的价值是多少？
+    class knapsack0_1:
+        # N=3
+        # W=5
+        # wt = [2, 1, 3]
+        # val = [4, 2, 3]
+        
+        def knapsack(N, W, wt, val):
+            dp = np.zeros((N+1, W+1))   #初始化dp二维数组
+            for i in range(1, N+1):     #对N个物品进行选择
+                for w in range(1, W+1):    #对i个物品时计算每个W的最优解
+                    if w-wt[i-1]<0:          #如果背包装不下了，则不装如第i个物品
+                        dp[i][w]=dp[i-1][w]
+                    else:                     #能装下时，选择价值最大的装法
+                        dp[i][w]=max(dp[i-1][w], dp[i-1][w-wt[i-1]]+val[i-1])
+            return dp[N][W]
 
 if __name__ == "__main__":
     # print(Dynamic_programming.Fibonacci.fib(8))
     # print(Dynamic_programming.CoinChange.CoinChange([1,2,5],10))
+    print(Dynamic_programming.knapsack0_1.knapsack(3,4,[2,1,3],[4,2,3]))
+    
